4b287badb3307045e9979d1fce8367f0e256f9f0
Sezan Mahmud <sezanmahmud789@gmail.com>
2026-01-08 02:18:06 +0600

    Assignment - 2
    BMI Calculator Using Dart Functions

---
+++ assignment_2/bin/assignment_2.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+  double calculateBMI(double weight,double height){
+    return weight / (height * height);
+  }
+
+  String getBMICategory( double bmi){
+
+    if(bmi < 18.5){
+      return "Underweight";
+    }
+    else if(bmi >= 18.5 && bmi <= 24.9 ){
+      return "Normal";
+    }
+    else if(bmi >= 25 && bmi <= 29.9 ){
+      return "Overweight";
+    }
+    else{
+      return "Obese";
+    }
+  }
+
+void main(){
+     double weight = 73.0;
+     double height = 1.76;
+
+     double bmi = calculateBMI(weight, height);
+     String category = getBMICategory(bmi);
+
+     print("Your BMI is : ${bmi.toStringAsFixed(2)}");
+     print("Category : $category");
+}
